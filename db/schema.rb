db37526659d07e5b26889ab6b62ff3d1d7333f12
sagae keiga <sagae5.28rujeae@gmail.com>
2017-07-28 02:32:21 +0000

    寒河江の環境でbundle

--- db/schema.rb
+++ db/schema.rb
@@ -43,6 +43,20 @@ ActiveRecord::Schema.define(version: 20170727101050) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "recruits", force: :cascade do |t|
+    t.string "create_title"
+    t.string "period"
+    t.integer "people"
+    t.text "contents"
+    t.string "work_method"
+    t.string "communication"
+    t.string "wanted_jobs"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["create_title"], name: "index_recruits_on_create_title", unique: true
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "user_name", default: "", null: false
     t.string "email", default: "", null: false
@@ -63,7 +77,6 @@ ActiveRecord::Schema.define(version: 20170727101050) do
     t.string "unlock_token"
     t.datetime "locked_at"
     t.text "self_introduction"
-    t.string "language_used"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
