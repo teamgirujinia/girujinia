51d23a29fb127a08ddf145dcdb4ac9bbacf52fe7
sagae keiga <sagae5.28rujeae@gmail.com>
2017-07-31 04:12:34 +0000

    seedデータの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,18 @@
+# ユーザーの作成
+
 User.create!(user_name:  "相馬",
              email: "souma@gmail.com",
-             password:              "souma",
-             password_confirmation: "souma")
+             password:              "soumasouma",
+             password_confirmation: "soumasouma")
+             
+             
+User.create!(user_name:  "寒河江",
+             email: "sagae5.28rujeae@gmail.com",
+             password:              "s19930528",
+             password_confirmation: "s19930528")
+             
+User.create!(user_name:  "girujinia",
+             email: "girujinia@gmail.com",
+             password:              "sgirujinia",
+             password_confirmation: "sgirujinia")
+             
